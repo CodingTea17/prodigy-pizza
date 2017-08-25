ba2ee58ca89f39191bb8f83ea72f67f060e74498
Dawson M <dawson.mortenson@gmail.com>
2017-08-25 15:20:41 -0700

    refactor the pizza size selection section

--- js/scripts.js
+++ js/scripts.js
@@ -26,15 +26,17 @@ $(document).ready(function(){
     $("#order-pizza").css({"visibility":"visible"});
     $("#pizza-size-radio").show();
     // $("#pizza-topping-checkbox").hide();
+    var crustSize = "3";
     $("#pizza-maker input").change(function() {
-      if($("input[name=pizzaSize]:checked").val() === "3"){
+      crustSize = parseInt($("input[name=pizzaSize]:checked").val());
+      if(crustSize === 3){
         $(".circle").css({"height": "450px", "width":"450px"});
-      } else if($("input[name=pizzaSize]:checked").val() === "2"){
+      } else if(crustSize === 2){
         $(".circle").css({"height": "400px", "width":"400px"});
-      } else if($("input[name=pizzaSize]:checked").val() === "1"){
+      } else if(crustSize === 1){
         $(".circle").css({"height": "350px", "width":"350px"});
-      } else if($("input[name=pizzaSize]:checked").val() === "0"){
-        $(".circle").css({"height": "250px", "width":"250px"});
+      } else if(crustSize === 0){
+        $(".circle").css({"height": "300px", "width":"300px"});
       }
     });
     $("#next-size").click(function(){
@@ -43,6 +45,7 @@ $(document).ready(function(){
       // Code to append selected toppings to a list?
 
       $("#next-toppings").click(function(){
+
         $("#pizza-topping-checkbox").hide();
         $("#pizza-summary").show();
       });
