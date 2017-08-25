6f2d0ef69d3070e217ad8fa9a39ceaf7ebcb009c
Dawson M <dawson.mortenson@gmail.com>
2017-08-25 13:52:28 -0700

    add frontend logic for the choose pizza size page

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,20 @@ $(document).ready(function(){
   $("#order").click(function(){
     $("#main-page").hide();
     $("#order-pizza").show();
+    $("#pizza-maker input").change(function() {
+      if($("input[name=pizzaSize]:checked").val() === "3"){
+        $(".circle").css({"height": "400px", "width":"400px"});
+      } else if($("input[name=pizzaSize]:checked").val() === "2"){
+        $(".circle").css({"height": "350px", "width":"350px"});
+      } else if($("input[name=pizzaSize]:checked").val() === "1"){
+        $(".circle").css({"height": "300px", "width":"300px"});
+      } else if($("input[name=pizzaSize]:checked").val() === "0"){
+        $(".circle").css({"height": "250px", "width":"250px"});
+      }
+    });
+    $("#next-size").click(function(){
+
+    })
   });
 
 });
