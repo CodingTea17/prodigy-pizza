ea27ceaf3932cf6d7c85f6fd030de22e7a11c862
Dawson M <dawson.mortenson@gmail.com>
2017-08-25 16:14:10 -0700

    add a final price calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,30 @@
 // Backend Logic
-function Pizza(size, toppings) {
-  this.size = size;
+function Pizza(crust, toppings) {
+  this.crust = crust;
   this.toppings = toppings;
   this.finalPrice = 0;
 }
-
+function Crust(name, diameter) {
+  this.name = name;
+  this.diameter = diameter;
+}
 Pizza.prototype.pizzaPriceCalulator = function () {
-  var addUpPrice = 0;
+  var totalPrice = 0;
+  var numberOfToppings = 0;
   // Adds the base price of the crust which is $1 per inch in diameter
-  addUpPrice += parseInt(this.size);
-
-
+  totalPrice += this.crust.crustSize();
+  // alert(this.toppings);
+  this.toppings.forEach(function(){
+    numberOfToppings += 1;
+  });
+  totalPrice += numberOfToppings * (this.crust.crustSize() / 10);
+  totalPrice = Number(totalPrice).toFixed(2);  
+  return totalPrice;
 };
+
+Crust.prototype.crustSize = function() {
+  return this.diameter;
+}
 
 
 
@@ -20,23 +33,31 @@ Pizza.prototype.pizzaPriceCalulator = function () {
 
 // Frontend Logic
 $(document).ready(function(){
+  var pizzaCrust;
+  var pizza;
+  var toppingsSelection = [];
+  var crustSize = 3;
+
   // Mock changes page to order page
   $("#order").click(function(){
     $("#main-page").hide();
     $("#order-pizza").css({"visibility":"visible"});
     $("#pizza-size-radio").show();
     // $("#pizza-topping-checkbox").hide();
-    var crustSize = "3";
     $("#pizza-maker input").change(function() {
       crustSize = parseInt($("input[name=pizzaSize]:checked").val());
       if(crustSize === 3){
         $(".circle").css({"height": "450px", "width":"450px"});
+        pizzaCrust = new Crust("Jumbo", 16);
       } else if(crustSize === 2){
         $(".circle").css({"height": "400px", "width":"400px"});
+        pizzaCrust = new Crust("Large", 14);
       } else if(crustSize === 1){
         $(".circle").css({"height": "350px", "width":"350px"});
+        pizzaCrust = new Crust("Medium", 12);
       } else if(crustSize === 0){
         $(".circle").css({"height": "300px", "width":"300px"});
+        pizzaCrust = new Crust("Small", 10);
       }
     });
     $("#next-size").click(function(){
@@ -45,9 +66,23 @@ $(document).ready(function(){
       // Code to append selected toppings to a list?
 
       $("#next-toppings").click(function(){
+        $("#crust-size").text(pizzaCrust.name);
+        $('input[name=pizzaToppings]:checked').each(function(i){
+            $("#pizza-summary-toppings").append("<li>" + $(this).val() + "</li>");
+            toppingsSelection.push($(this).val());
+        });
+
+        pizza = new Pizza(pizzaCrust, toppingsSelection);
+
+        $("#final-price").text(pizza.pizzaPriceCalulator());
+
 
         $("#pizza-topping-checkbox").hide();
         $("#pizza-summary").show();
+        $("form#pizza-maker").submit(function(event){
+          event.preventDefault();
+          $("#order-confirmation-page").show();
+        })
       });
     });
   });
