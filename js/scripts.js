78e1fdee6037abc0aa97a4b62b69c404dd89138b
Dawson M <dawson.mortenson@gmail.com>
2017-08-25 13:22:37 -0700

    update the font and add content to the order page

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// Backend Logic
+
+
+
+
+
+
+
+
+// Frontend Logic
+$(document).ready(function(){
+  // Mock changes page to order page
+  $("#order").click(function(){
+    $("#main-page").hide();
+    $("#order-pizza").show();
+  });
+
+});
